ebc8570bdafc93cbd3bb3a89a22c784c86efd9a8
Dimi Catrysse <dimicatrysse@outlook.com>
2021-11-07 14:11:20 +0100

    Refactored code + return name and description of devices

--- src/ttn/index.ts
+++ src/ttn/index.ts
@@ -24,7 +24,7 @@ export { EndDevice }
 function generate_error(err: any, origin: string) {
     return {
         message: err.message, 
-        details: err.response.data,
+        details: err.response.data.message,
         origin
     }
 }
@@ -33,14 +33,24 @@ const ttn = {
     devices: {
         list: (application_id: string) => {
             return new Promise<any>((resolve, reject) => {
-                identity_server.get(`/applications/${application_id}/devices`)
+                identity_server.get(
+                    `/applications/${application_id}/devices`, 
+                    { 
+                        params: { field_mask: 'name,description' }
+                    }
+                )
                 .then( response => resolve(response.data) )
                 .catch( err => reject(generate_error(err, 'identity_server')) )
             })
         },
         get: (application_id: string, device_id: string) => {
             return new Promise<any>((resolve, reject) => {
-                identity_server.get(`/applications/${application_id}/devices/${device_id}`)
+                identity_server.get(
+                    `/applications/${application_id}/devices/${device_id}`, 
+                    { 
+                        params: { field_mask: 'name,description' }
+                    }
+                )
                 .then( response => resolve(response.data) )
                 .catch( err => reject(generate_error(err, 'identity_server')) )
             })
@@ -48,40 +58,52 @@ const ttn = {
         create: (end_device: EndDevice) => {
             function create_in_identity_server(end_device: EndDevice) {
                 return new Promise<any>((resolve, reject) => {
-                    identity_server.post(`/applications/${end_device.ids.application_ids.application_id}/devices`, {
-                        end_device,
-                        field_mask: { paths: field_mask_paths.identity_server }
-                    })
+                    identity_server.post(
+                        `/applications/${end_device.ids.application_ids.application_id}/devices`,
+                        {
+                            end_device,
+                            field_mask: { paths: field_mask_paths.identity_server }
+                        }
+                    )
                     .then( response => resolve(response.data) )
                     .catch( err => reject(generate_error(err, 'identity_server')) )
                 })
             }
             function create_in_network_server(end_device: EndDevice) {
                 return new Promise<any>((resolve, reject) => {
-                    network_server.post(`/applications/${end_device.ids.application_ids.application_id}/devices`, {
-                        end_device,
-                        field_mask: { paths: field_mask_paths.network_server }
-                    })
+                    network_server.post(
+                        `/applications/${end_device.ids.application_ids.application_id}/devices`, 
+                        {
+                            end_device,
+                            field_mask: { paths: field_mask_paths.network_server }
+                        }
+                    )
                     .then( response => resolve(response.data) )
                     .catch( err => reject(generate_error(err, 'network_server')) )
                 })
             }
             function create_in_application_server(end_device: EndDevice) {
                 return new Promise<any>((resolve, reject) => {
-                    application_server.post(`/applications/${end_device.ids.application_ids.application_id}/devices`, {
-                        end_device,
-                        field_mask: { paths: field_mask_paths.application_server }
-                    })
+                    application_server.post(
+                        `/applications/${end_device.ids.application_ids.application_id}/devices`, 
+                        {
+                            end_device,
+                            field_mask: { paths: field_mask_paths.application_server }
+                        }
+                    )
                     .then( response => resolve(response.data) )
                     .catch( err => reject(generate_error(err, 'application_server')) )
                 })
             }
             function create_in_join_server(end_device: EndDevice) {
                 return new Promise<any>((resolve, reject) => {
-                    join_server.post(`/applications/${end_device.ids.application_ids.application_id}/devices`, {
-                        end_device,
-                        field_mask: { paths: field_mask_paths.join_server }
-                    })
+                    join_server.post(
+                        `/applications/${end_device.ids.application_ids.application_id}/devices`, 
+                        {
+                            end_device,
+                            field_mask: { paths: field_mask_paths.join_server }
+                        }
+                    )
                     .then( response => resolve(response.data) )
                     .catch( err => reject(generate_error(err, 'join_server')) )
                 })
@@ -94,16 +116,9 @@ const ttn = {
                     create_in_application_server(end_device),
                     create_in_join_server(end_device)
                 ])
-                .then((results) => {
-                    /*
-                    let created = {}
-                    for (const result of results) {
-                        created = {...created, ...result}
-                    }
-                    resolve(created);
-                    */
+                .then( () => {
                     ttn.devices.get(end_device.ids.application_ids.application_id, end_device.ids.device_id)
-                    .then( (device) => resolve(device) )
+                    .then( device => resolve(device) )
                     .catch( err => reject(err) )
                 })
                 .catch( err => reject(err) )
@@ -112,28 +127,36 @@ const ttn = {
         delete: (application_id: string, device_id: string) => {
             function delete_in_identity_server(application_id: string, device_id: string) {
                 return new Promise<any>((resolve, reject) => {
-                    identity_server.delete(`/applications/${application_id}/devices/${device_id}`)
+                    identity_server.delete(
+                        `/applications/${application_id}/devices/${device_id}`
+                    )
                     .then( response => resolve(response.data) )
                     .catch( err => reject(generate_error(err, 'identity_server')) )
                 })
             }
             function delete_in_join_server(application_id: string, device_id: string) {
                 return new Promise<any>((resolve, reject) => {
-                    join_server.delete(`/applications/${application_id}/devices/${device_id}`)
+                    join_server.delete(
+                        `/applications/${application_id}/devices/${device_id}`
+                    )
                     .then( response => resolve(response.data) )
                     .catch( err => reject(generate_error(err, 'join_server')) )
                 })
             }
             function delete_in_application_server(application_id: string, device_id: string) {
                 return new Promise<any>((resolve, reject) => {
-                    application_server.delete(`/applications/${application_id}/devices/${device_id}`)
+                    application_server.delete(
+                        `/applications/${application_id}/devices/${device_id}`
+                    )
                     .then( response => resolve(response.data) )
                     .catch( err => reject(generate_error(err, 'application_server')) )
                 })
             }
             function delete_in_network_server(application_id: string, device_id: string) {
                 return new Promise<any>((resolve, reject) => {
-                    network_server.delete(`/applications/${application_id}/devices/${device_id}`)
+                    network_server.delete(
+                        `/applications/${application_id}/devices/${device_id}`
+                    )
                     .then( response => resolve(response.data) )
                     .catch( err => reject(generate_error(err, 'network_server')) )
                 })
